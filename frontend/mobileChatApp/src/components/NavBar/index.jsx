817227e9f3d4c4b1d664142d01a47b65b1a1ef4c
Samuli Toppi <samuli.toppi@outlook.com>
2023-10-02 17:58:40 +0300

    Signing out works

--- frontend/mobileChatApp/src/components/NavBar/index.jsx
+++ frontend/mobileChatApp/src/components/NavBar/index.jsx
@@ -18,13 +18,25 @@ const NavBar = () => {
   const data = useGetCurrentUser()
   user = data && data.user
 
+  user ? console.log(user) : console.log('No one here')
+
+  const handleSignOut = async () => {
+    try {
+      await authStorage.removeAccessToken()
+      apolloClient.resetStore()
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <NavBarTab text='Sign in' onPress={() => navigate('/sign-in')} />
         <NavBarTab text='Sign up' onPress={() => console.log('sign up')} />
         {user && (
-          <NavBarTab text='Sign out' />
+          <NavBarTab text='Sign out' onPress={handleSignOut} />
         )}
 
       </ScrollView>
