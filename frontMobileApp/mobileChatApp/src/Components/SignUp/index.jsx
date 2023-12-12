ba02020ed225ac68e2dcf954e56aa2607d997b13
Samuli Toppi <samuli.toppi@outlook.com>
2023-12-12 16:56:17 +0200

    Image can be picked

--- frontMobileApp/mobileChatApp/src/Components/SignUp/index.jsx
+++ frontMobileApp/mobileChatApp/src/Components/SignUp/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { Pressable, Text, TextInput, Image, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+import { View, Pressable, Text, TextInput, Image, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import CustomButton from '../SignIn/CustomButton'
-import { launchImageLibrary } from 'react-native-image-picker'
+import * as ImagePicker from 'expo-image-picker'
 
 
 
-const SignUp = () => {
+const SignUp =  () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
@@ -16,16 +16,19 @@ const SignUp = () => {
 
   const [image, setImage] = useState(null)
 
-  const selectImage = () => {
-    const options = {
-      noData: true,
-    }
-
-    launchImageLibrary(options, response => {
-      if (response.assets && response.assets[0].uri) {
-        setImage(response.assets[0].uri)
-      }
+  const selectImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     })
+
+    console.log(result)
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri)
+    }
   }
 
   const handleSignUp = () => {
@@ -39,81 +42,75 @@ const SignUp = () => {
   }
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}
-    >
-      <ScrollView contentContainerStyle={styles.container}>
-        <Pressable style={styles.imagePicker} onPress={selectImage}>
-          {image ? (
-            <Image source={{ uri: image }} style={styles.image} />
-          ) : (
-            <Text style={styles.imagePickerText}>+</Text>
-          )}
-        </Pressable>
-        <Text style={styles.title}>Choose your username</Text>
-        <Text>Username:</Text>
-        <TextInput
-          style={styles.input}
-          value={username}
-          onChangeText={setUsername}
-          placeholder='Username'
-        />
-        <Text>Password:</Text>
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          placeholder='Password'
-        />
-        <Text>Confirm Password:</Text>
-        <TextInput
-          style={styles.input}
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-          secureTextEntry
-          placeholder='Confirm Password'
-        />
-        <Text style={styles.title}>Additional information</Text>
-        <Text>Name:</Text>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholder='Name'
-        />
-        <Text>Phone:</Text>
-        <TextInput
-          style={styles.input}
-          value={phone}
-          onChangeText={setPhone}
-          placeholder='Phone'
-          keyboardType='phone-pad'
-        />
-        <Text>City:</Text>
-        <TextInput
-          style={styles.input}
-          value={city}
-          onChangeText={setCity}
-          placeholder='City'
-        />
-        <Text>Profile Picture URL:</Text>
-        <TextInput
-          style={styles.input}
-          value={profilePicture}
-          onChangeText={setProfilePicture}
-          placeholder='Profile Picture URL'
-        />
-        <CustomButton onPress={handleSignUp} title='Register' />
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <ScrollView contentContainerStyle={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <View>
+          <Pressable style={styles.imagePicker} onPress={selectImage}>
+            {image ? (
+              <Image source={{ uri: image }} style={styles.image} />
+            ) : (
+              <Text style={styles.imagePickerText}>+</Text>
+            )}
+          </Pressable>
+          <Text style={styles.title}>Choose your username</Text>
+          <Text>Username:</Text>
+          <TextInput
+            style={styles.input}
+            value={username}
+            onChangeText={setUsername}
+            placeholder='Username'
+          />
+          <Text>Password:</Text>
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            placeholder='Password'
+          />
+          <Text>Confirm Password:</Text>
+          <TextInput
+            style={styles.input}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+            placeholder='Confirm Password'
+          />
+          <Text style={styles.title}>Additional information</Text>
+          <Text>Name:</Text>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            placeholder='Name'
+          />
+          <Text>Phone:</Text>
+          <TextInput
+            style={styles.input}
+            value={phone}
+            onChangeText={setPhone}
+            placeholder='Phone'
+            keyboardType='phone-pad'
+          />
+          <Text>City:</Text>
+          <TextInput
+            style={styles.input}
+            value={city}
+            onChangeText={setCity}
+            placeholder='City'
+          />
+          <CustomButton onPress={handleSignUp} title='Register' />
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     padding: 16,
   },
