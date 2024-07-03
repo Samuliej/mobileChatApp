38d39b1634220f9e790b811b112a865462e41f1b
Samuli Toppi <samuli.toppi@outlook.com>
2024-07-03 16:35:27 +0300

    User can only like a post once

--- backend-express/models/Post.js
+++ backend-express/models/Post.js
@@ -30,6 +30,7 @@ const postSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
+  likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
